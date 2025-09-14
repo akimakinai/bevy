f384fe55bdb86460f34e64c9c82ae08d981dbda2
akimakinai <105044389+akimakinai@users.noreply.github.com>
2025-09-14 23:26:15 +0900

    Make sprite picking aware of render layers

--- crates/bevy_sprite/src/picking_backend.rs
+++ crates/bevy_sprite/src/picking_backend.rs
@@ -13,7 +13,10 @@
 use crate::{Anchor, Sprite};
 use bevy_app::prelude::*;
 use bevy_asset::prelude::*;
-use bevy_camera::{visibility::ViewVisibility, Camera, Projection};
+use bevy_camera::{
+    visibility::{RenderLayers, ViewVisibility},
+    Camera, Projection,
+};
 use bevy_color::Alpha;
 use bevy_ecs::prelude::*;
 use bevy_image::prelude::*;
@@ -88,6 +91,7 @@ fn sprite_picking(
         &GlobalTransform,
         &Projection,
         Has<SpritePickingCamera>,
+        Option<&RenderLayers>,
     )>,
     primary_window: Query<Entity, With<PrimaryWindow>>,
     images: Res<Assets<Image>>,
@@ -99,15 +103,16 @@ fn sprite_picking(
         &GlobalTransform,
         &Anchor,
         &Pickable,
+        Option<&RenderLayers>,
         &ViewVisibility,
     )>,
     mut pointer_hits_writer: MessageWriter<PointerHits>,
 ) {
     let mut sorted_sprites: Vec<_> = sprite_query
         .iter()
-        .filter_map(|(entity, sprite, transform, anchor, pickable, vis)| {
+        .filter_map(|(entity, sprite, transform, anchor, pickable, layers, vis)| {
             if !transform.affine().is_nan() && vis.get() {
-                Some((entity, sprite, transform, anchor, pickable))
+                Some((entity, sprite, transform, anchor, layers, pickable))
             } else {
                 None
             }
@@ -115,7 +120,7 @@ fn sprite_picking(
         .collect();
 
     // radsort is a stable radix sort that performed better than `slice::sort_by_key`
-    radsort::sort_by_key(&mut sorted_sprites, |(_, _, transform, _, _)| {
+    radsort::sort_by_key(&mut sorted_sprites, |(_, _, transform, _, _, _)| {
         -transform.translation().z
     });
 
@@ -125,19 +130,25 @@ fn sprite_picking(
         pointer_location.location().map(|loc| (pointer, loc))
     }) {
         let mut blocked = false;
-        let Some((cam_entity, camera, cam_transform, Projection::Orthographic(cam_ortho), _)) =
-            cameras
-                .iter()
-                .filter(|(_, camera, _, _, cam_can_pick)| {
-                    let marker_requirement = !settings.require_markers || *cam_can_pick;
-                    camera.is_active && marker_requirement
-                })
-                .find(|(_, camera, _, _, _)| {
-                    camera
-                        .target
-                        .normalize(primary_window)
-                        .is_some_and(|x| x == location.target)
-                })
+        let Some((
+            cam_entity,
+            camera,
+            cam_transform,
+            Projection::Orthographic(cam_ortho),
+            _,
+            camera_layers,
+        )) = cameras
+            .iter()
+            .filter(|(_, camera, _, _, cam_can_pick, _)| {
+                let marker_requirement = !settings.require_markers || *cam_can_pick;
+                camera.is_active && marker_requirement
+            })
+            .find(|(_, camera, _, _, _, _)| {
+                camera
+                    .target
+                    .normalize(primary_window)
+                    .is_some_and(|x| x == location.target)
+            })
         else {
             continue;
         };
@@ -150,6 +161,8 @@ fn sprite_picking(
             continue;
         }
 
+        let camera_layers = camera_layers.unwrap_or_default();
+
         let Ok(cursor_ray_world) = camera.viewport_to_world(cam_transform, viewport_pos) else {
             continue;
         };
@@ -159,95 +172,102 @@ fn sprite_picking(
         let picks: Vec<(Entity, HitData)> = sorted_sprites
             .iter()
             .copied()
-            .filter_map(|(entity, sprite, sprite_transform, anchor, pickable)| {
-                if blocked {
-                    return None;
-                }
-
-                // Transform cursor line segment to sprite coordinate system
-                let world_to_sprite = sprite_transform.affine().inverse();
-                let cursor_start_sprite = world_to_sprite.transform_point3(cursor_ray_world.origin);
-                let cursor_end_sprite = world_to_sprite.transform_point3(cursor_ray_end);
-
-                // Find where the cursor segment intersects the plane Z=0 (which is the sprite's
-                // plane in sprite-local space). It may not intersect if, for example, we're
-                // viewing the sprite side-on
-                if cursor_start_sprite.z == cursor_end_sprite.z {
-                    // Cursor ray is parallel to the sprite and misses it
-                    return None;
-                }
-                let lerp_factor =
-                    f32::inverse_lerp(cursor_start_sprite.z, cursor_end_sprite.z, 0.0);
-                if !(0.0..=1.0).contains(&lerp_factor) {
-                    // Lerp factor is out of range, meaning that while an infinite line cast by
-                    // the cursor would intersect the sprite, the sprite is not between the
-                    // camera's near and far planes
-                    return None;
-                }
-                // Otherwise we can interpolate the xy of the start and end positions by the
-                // lerp factor to get the cursor position in sprite space!
-                let cursor_pos_sprite = cursor_start_sprite
-                    .lerp(cursor_end_sprite, lerp_factor)
-                    .xy();
-
-                let Ok(cursor_pixel_space) = sprite.compute_pixel_space_point(
-                    cursor_pos_sprite,
-                    *anchor,
-                    &images,
-                    &texture_atlas_layout,
-                ) else {
-                    return None;
-                };
-
-                // Since the pixel space coordinate is `Ok`, we know the cursor is in the bounds of
-                // the sprite.
-
-                let cursor_in_valid_pixels_of_sprite = 'valid_pixel: {
-                    match settings.picking_mode {
-                        SpritePickingMode::AlphaThreshold(cutoff) => {
-                            let Some(image) = images.get(&sprite.image) else {
-                                // [`Sprite::from_color`] returns a defaulted handle.
-                                // This handle doesn't return a valid image, so returning false here would make picking "color sprites" impossible
-                                break 'valid_pixel true;
-                            };
-                            // grab pixel and check alpha
-                            let Ok(color) = image.get_color_at(
-                                cursor_pixel_space.x as u32,
-                                cursor_pixel_space.y as u32,
-                            ) else {
-                                // We don't know how to interpret the pixel.
-                                break 'valid_pixel false;
-                            };
-                            // Check the alpha is above the cutoff.
-                            color.alpha() > cutoff
-                        }
-                        SpritePickingMode::BoundingBox => true,
+            .filter_map(
+                |(entity, sprite, sprite_transform, anchor, sprite_layers, pickable)| {
+                    if blocked {
+                        return None;
                     }
-                };
 
-                blocked = cursor_in_valid_pixels_of_sprite && pickable.should_block_lower;
+                    if !camera_layers.intersects(sprite_layers.unwrap_or_default()) {
+                        return None;
+                    }
 
-                cursor_in_valid_pixels_of_sprite.then(|| {
-                    let hit_pos_world =
-                        sprite_transform.transform_point(cursor_pos_sprite.extend(0.0));
-                    // Transform point from world to camera space to get the Z distance
-                    let hit_pos_cam = cam_transform
-                        .affine()
-                        .inverse()
-                        .transform_point3(hit_pos_world);
-                    // HitData requires a depth as calculated from the camera's near clipping plane
-                    let depth = -cam_ortho.near - hit_pos_cam.z;
-                    (
-                        entity,
-                        HitData::new(
-                            cam_entity,
-                            depth,
-                            Some(hit_pos_world),
-                            Some(*sprite_transform.back()),
-                        ),
-                    )
-                })
-            })
+                    // Transform cursor line segment to sprite coordinate system
+                    let world_to_sprite = sprite_transform.affine().inverse();
+                    let cursor_start_sprite =
+                        world_to_sprite.transform_point3(cursor_ray_world.origin);
+                    let cursor_end_sprite = world_to_sprite.transform_point3(cursor_ray_end);
+
+                    // Find where the cursor segment intersects the plane Z=0 (which is the sprite's
+                    // plane in sprite-local space). It may not intersect if, for example, we're
+                    // viewing the sprite side-on
+                    if cursor_start_sprite.z == cursor_end_sprite.z {
+                        // Cursor ray is parallel to the sprite and misses it
+                        return None;
+                    }
+                    let lerp_factor =
+                        f32::inverse_lerp(cursor_start_sprite.z, cursor_end_sprite.z, 0.0);
+                    if !(0.0..=1.0).contains(&lerp_factor) {
+                        // Lerp factor is out of range, meaning that while an infinite line cast by
+                        // the cursor would intersect the sprite, the sprite is not between the
+                        // camera's near and far planes
+                        return None;
+                    }
+                    // Otherwise we can interpolate the xy of the start and end positions by the
+                    // lerp factor to get the cursor position in sprite space!
+                    let cursor_pos_sprite = cursor_start_sprite
+                        .lerp(cursor_end_sprite, lerp_factor)
+                        .xy();
+
+                    let Ok(cursor_pixel_space) = sprite.compute_pixel_space_point(
+                        cursor_pos_sprite,
+                        *anchor,
+                        &images,
+                        &texture_atlas_layout,
+                    ) else {
+                        return None;
+                    };
+
+                    // Since the pixel space coordinate is `Ok`, we know the cursor is in the bounds of
+                    // the sprite.
+
+                    let cursor_in_valid_pixels_of_sprite = 'valid_pixel: {
+                        match settings.picking_mode {
+                            SpritePickingMode::AlphaThreshold(cutoff) => {
+                                let Some(image) = images.get(&sprite.image) else {
+                                    // [`Sprite::from_color`] returns a defaulted handle.
+                                    // This handle doesn't return a valid image, so returning false here would make picking "color sprites" impossible
+                                    break 'valid_pixel true;
+                                };
+                                // grab pixel and check alpha
+                                let Ok(color) = image.get_color_at(
+                                    cursor_pixel_space.x as u32,
+                                    cursor_pixel_space.y as u32,
+                                ) else {
+                                    // We don't know how to interpret the pixel.
+                                    break 'valid_pixel false;
+                                };
+                                // Check the alpha is above the cutoff.
+                                color.alpha() > cutoff
+                            }
+                            SpritePickingMode::BoundingBox => true,
+                        }
+                    };
+
+                    blocked = cursor_in_valid_pixels_of_sprite && pickable.should_block_lower;
+
+                    cursor_in_valid_pixels_of_sprite.then(|| {
+                        let hit_pos_world =
+                            sprite_transform.transform_point(cursor_pos_sprite.extend(0.0));
+                        // Transform point from world to camera space to get the Z distance
+                        let hit_pos_cam = cam_transform
+                            .affine()
+                            .inverse()
+                            .transform_point3(hit_pos_world);
+                        // HitData requires a depth as calculated from the camera's near clipping plane
+                        let depth = -cam_ortho.near - hit_pos_cam.z;
+                        (
+                            entity,
+                            HitData::new(
+                                cam_entity,
+                                depth,
+                                Some(hit_pos_world),
+                                Some(*sprite_transform.back()),
+                            ),
+                        )
+                    })
+                },
+            )
             .collect();
 
         let order = camera.order as f32;
