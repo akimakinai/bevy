4e91021d407b5cfeb6c4cb2ee3445ee4c15cd85b
akmkni <105044389+akimakinai@users.noreply.github.com>
2026-06-20 09:05:33 +0900

    Fix table in bsn macro doc

--- crates/bevy_scene/src/lib.rs
+++ crates/bevy_scene/src/lib.rs
@@ -963,7 +963,7 @@ use bevy_ecs::prelude::*;
 /// | `MyEnum::Variant`                          | Enum Component `MyEnum` with the `Variant` variant                                                             |
 /// | `template_value(component)`                | Insert the component value from a variable `component`                                                         |
 /// | `template_value(CompA::from_str("foo"))`   | Insert the component value by immediately calling the constructor                                              |
-/// | `template(|context| { ... })`              | Register a function/closure returning a Template (eg. Component). Its passed [`context`] allowing World access |
+/// | `template(\|context\| { ... })`            | Register a function/closure returning a Template (eg. Component). Its passed [`context`] allowing World access |
 /// | `~MyType`<br>`~MyType {name: var}`         | Type implementing [`Template`], the prefix is used to distinguish it from Components which use [`FromTemplate`]|
 /// | **Including Scenes**                       |                                                                                                                |
 /// | `scene()`<br>`scene(val)`                  | Include the result of a `impl `[`Scene`] function                                                              |
